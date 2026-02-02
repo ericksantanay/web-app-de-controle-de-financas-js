30b71cd2416db904337e6384b3d1e0d7cd9bd7c8
Erick Silva <donizttierick@gmail.com>
2026-02-02 14:10:28 -0300

    Ajustes

--- assets/js/abas.js
+++ assets/js/abas.js
@@ -2,13 +2,14 @@
 const header = document.getElementById('cabecalho')
 const PrimeiraSecao = document.getElementById('secao-do-home')
 const terceiraSecao = document.getElementById('secao-adicionar-gasto')
+const secaoPerfil = document.getElementById('secao-perfil')
 
 
 //SAIDAS DAS CORES 
 const menuInicio = document.getElementById('menuInicio')
 const historicMoviment = document.getElementById('historico-movimentacao')
 const movimentacaoAdicionar = document.getElementById('movimentacao-adicionar')
-
+const Perfil = document.querySelector('.menu')
 
 
 menuInicio.style.backgroundColor = '#6d28d9'
@@ -17,19 +18,22 @@ function inicioMenu() {
     menuInicio.style.backgroundColor = '#6d28d9'
     historicMoviment.style.backgroundColor = 'white'
     movimentacaoAdicionar.style.backgroundColor = 'white'
+    Perfil.style.backgroundColor = 'white'
 
      // AQUI SAO OS QUE VAO APARECER 
     header.style.display = 'block'
     PrimeiraSecao.style.display = 'block'
     terceiraSecao.style.display = 'none'
+    secaoPerfil.style.display = 'none'
     
 }
 
 
-function adicionarMovimentacaoMenu() {
+function historicoMenu() {
     historicMoviment.style.backgroundColor = '#6d28d9'
     menuInicio.style.backgroundColor = 'white'
     movimentacaoAdicionar.style.backgroundColor = 'white'
+    Perfil.style.backgroundColor = 'white'
 }
 
 
@@ -37,10 +41,33 @@ function adicionarMovimentacaoMenu() {
     movimentacaoAdicionar.style.backgroundColor = '#6d28d9'
     historicMoviment.style.backgroundColor = 'white'
     menuInicio.style.backgroundColor = 'white'
+    Perfil.style.backgroundColor = 'white'
 
-    // AQUI SAO OS QUE VAO APARECER 
+    
     terceiraSecao.style.display = 'block'
     header.style.display = 'none'
     PrimeiraSecao.style.display = 'none'
+    
 
+}
+
+
+
+
+function perfilMenu() {
+    Perfil.style.backgroundColor = '#6d28d9'
+    movimentacaoAdicionar.style.backgroundColor = '#white'
+    historicMoviment.style.backgroundColor = 'white'
+    menuInicio.style.backgroundColor = 'white'
+
+
+
+
+
+    // AQUI SAO OS QUE VAO APARECER 
+    secaoPerfil.style.display = 'block'
+    header.style.display = 'none'
+    PrimeiraSecao.style.display = 'none'
+    terceiraSecao.style.display = 'none'
+    
 }
